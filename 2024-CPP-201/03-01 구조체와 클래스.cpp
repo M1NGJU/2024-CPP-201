148cae8fb6f00ce54e02882f6aa2bcbf6bc6f9ac
M1NGJU <s2303@e-mirim.hs.kr>
2024-04-08 14:40:19 +0900

    구조체는 default가 public

---
+++ 2024-CPP-201/03-01 구조체와 클래스.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+//구조체는 디폴트가 public
+struct Student {
+	int hakbun_;
+	string name_;
+
+};
+void main(void) {
+	struct Student juyoung;
+	juyoung.hakbun_ = 2155;
+	juyoung.name_ = "윤주영";
+}
