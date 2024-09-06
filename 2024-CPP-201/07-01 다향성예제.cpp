9789e11fe9f3833f5890ddbe332697dcf0de4f97
M1NGJU <s2303@e-mirim.hs.kr>
2024-09-06 09:08:17 +0900

    프론트와 attack()일부 구현

--- 2024-CPP-201/07-01 다향성예제.cpp
+++ 2024-CPP-201/07-01 다향성예제.cpp
@@ -14,7 +14,12 @@ public:
         cout << "만드는 시간 : " << making_time_<< endl;
         cout << "아름다움 : " << beauty_ << endl;
     }
-private:
+
+    //자식 클래스에서 구현겠다
+    virtual void attack(Clothes* target) = 0;
+
+
+public:
     string name_;
     int price_;
     int making_time_;
@@ -26,12 +31,17 @@ public:
     Hanbok(string name, int price, int making_time, int beauty, int norigae, int jugori)
         : Clothes(name, price, making_time, beauty), norigae_(norigae), jugori_(jugori)
     {}
+
+    void attack(Clothes* target) {
+        target->beauty_ -= beauty_;
+    }
+
     void show() {
         Clothes::show();
         cout << "노리개 : " << norigae_ << endl;
         cout << "저고리 : " << jugori_ << endl;
     }
-private:
+public:
     int norigae_;        // 노리개
     int jugori_;        // 저고리
 };
@@ -45,7 +55,10 @@ public:
         Clothes::show();
         cout << "벨트 : " << belt_ << endl;
     }
-private:
+    void attack(Clothes* target) {
+        target->beauty_ -= beauty_;
+    }
+public:
     int belt_;            // 오비
 };
 
@@ -58,7 +71,10 @@ public:
         Clothes::show();
         cout << "자수 : " << embroidery_ << endl;
     }
-private:
+    void attack(Clothes* target) {
+        target->beauty_ -= beauty_;
+    }
+public:
     int embroidery_;    // 자수
 };
 
@@ -68,7 +84,14 @@ int main(void) {
     player->show();
     chingu->show();
 
+    cout << "---------------------------------" << endl;
+    cout << "1.공격" << endl;
+    cout << "2.특수공격1" << endl;
+    cout << "3.특수공격2" << endl;
+    cout << "4.도망" << endl;
+
     delete chingu;
     delete player;
+
 
 }
