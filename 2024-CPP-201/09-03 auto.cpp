573795f1e19f10411e0170a40f1c28221b64e72b
M1NGJU <s2303@e-mirim.hs.kr>
2024-10-14 13:48:26 +0900

    09-03 auto 적용 후

--- 2024-CPP-201/09-03 auto.cpp
+++ 2024-CPP-201/09-03 auto.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 
 void main(void) {
-	int a = 40;
+	auto a = 40;
 	cout << a << endl;
 
 	vector<string> v = { "난파란 사랑해요" ,  "이성호 사랑해", "박진서 사랑해"};
-	string b = v[2];
+	auto b = v[2];
 	cout << v[2] << endl;
 }
