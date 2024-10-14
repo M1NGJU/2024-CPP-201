4985d289bca16b72827bc116a2b80fb1feb498e1
M1NGJU <s2303@e-mirim.hs.kr>
2024-10-14 13:40:58 +0900

    09-03 auto 적용 전

---
+++ 2024-CPP-201/09-03 auto.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+#include<vector>
+using namespace std;
+
+
+void main(void) {
+	int a = 40;
+	cout << a << endl;
+
+	vector<string> v = { "난파란 사랑해요" ,  "이성호 사랑해", "박진서 사랑해"};
+	string b = v[2];
+	cout << v[2] << endl;
+}
